86edbb204d690968fc18e09cf95b50567624abcd
codinuum <codinuum@me.com>
2023-04-11 09:00:16 +0900

    diffviewer modified to work for git repositories

--- diffviewer/run.py
+++ diffviewer/run.py
@@ -15,10 +15,19 @@ if __name__ == '__main__':
                             formatter_class=ArgumentDefaultsHelpFormatter)
 
     parser.add_argument('file0', type=str, metavar='ORIGINAL',
-                        help='original source file')
+                        help='original source file (path or SHA1)')
 
     parser.add_argument('file1', type=str, metavar='MODIFIED',
-                        help='modified source file')
+                        help='modified source file (path or SHA1)')
+
+    parser.add_argument('--git', dest='git_repo', default=None,
+                        metavar='DIR', type=str, help='specify git repository path')
+
+    parser.add_argument('--path0', dest='path0', default=None,
+                        metavar='PATH', type=str, help='specify original file path')
+
+    parser.add_argument('--path', dest='path', default=None,
+                        metavar='PATH', type=str, help='specify modified file path')
 
     parser.add_argument('-c', '--cache', dest='cache', default=None,
                         metavar='DIR', type=str, help='specify cache path')
@@ -32,8 +41,6 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    apath0 = os.path.abspath(args.file0)
-    apath1 = os.path.abspath(args.file1)
     opt = ''
     if args.cache is not None:
         apathc = os.path.abspath(args.cache)
@@ -41,6 +48,20 @@ if __name__ == '__main__':
 
     if args.local_cache_name is not None:
         opt += f' --localcachename {args.local_cache_name}'
+
+    if args.git_repo is None:
+        apath0 = os.path.abspath(args.file0)
+        apath1 = os.path.abspath(args.file1)
+        opt += f' --file0 {apath0} --file1 {apath1}'
+    elif args.path is not None:
+        apathr = os.path.abspath(args.git_repo)
+        opt += f' --git {apathr} --oid0 {args.file0} --oid1 {args.file1} --path {args.path}'
+        if args.path0 is not None:
+            opt += f' --path0 {args.path0}'
+    else:
+        print('(modified) file path required')
+        parser.print_help()
+        sys.exit(1)
 
     here = os.path.dirname(sys.argv[0])
     viewer_app_path = None
@@ -54,10 +75,10 @@ if __name__ == '__main__':
     else:
         if args.foreground:
             cmdp = os.path.join(viewer_app_path, VIEWER_CMD_PATH)
-            cmd = f'{cmdp} --args{opt} --file0 {apath0} --file1 {apath1}'
+            cmd = f'{cmdp} --args{opt}'
         else:
-            app = os.path.join(viewer_app_path)
-            cmd = f'open -n {app} --args{opt} --file0 {apath0} --file1 {apath1}'
+            app = viewer_app_path
+            cmd = f'open -n {app} --args{opt}'
 
         try:
             rc = call(cmd, shell=True)
